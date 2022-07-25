574931a2a4d6583a908cd88f43dbbb7513f3041b
abdul3301 <abdulrahimsilvery@gmail.com>
2022-07-26 00:32:31 +0530

    Dummy Food App

---
+++ src/App.js
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { Meals } from "./Components/Meals/Meals";
+import Header from "./Components/Layout/Header";
+import Cart from "./Components/Cart/Cart";
+import CartProvider from "./store/CartProvider";
+
+function App() {
+  const [cartIsShown, setcartIsShown] = useState(false);
+
+  const showCartHandler = () => {
+    setcartIsShown(true);
+  };
+  const hideCartHandler = () => {
+    setcartIsShown(false);
+  };
+
+  return (
+    <div>
+      <CartProvider>
+        {cartIsShown && <Cart onClose={hideCartHandler} />}
+        <Header onShowCart={showCartHandler} />
+        <main>
+          <Meals />
+        </main>
+      </CartProvider>
+    </div>
+  );
+}
+
+export default App;
